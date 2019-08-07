5ef050cec9cc27b6b3ab001e0073f4539648cfe5
shmeegie <ericellb@gmail.com>
2019-08-07 19:13:09 -0400

    Updates

---
+++ precache-manifest.ca19780c9a2fc923e24c089641273ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf62293ef476d8f549d927429a3d4f1",
+    "url": "/Simple-Chat/index.html"
+  },
+  {
+    "revision": "1bd146232483d2fde3c8",
+    "url": "/Simple-Chat/static/css/main.a72d1aec.chunk.css"
+  },
+  {
+    "revision": "dd3848389aab81dd0638",
+    "url": "/Simple-Chat/static/js/2.9738973a.chunk.js"
+  },
+  {
+    "revision": "1bd146232483d2fde3c8",
+    "url": "/Simple-Chat/static/js/main.803295b6.chunk.js"
+  },
+  {
+    "revision": "de1605a253086a736309",
+    "url": "/Simple-Chat/static/js/runtime~main.29395272.js"
+  }
+]);
