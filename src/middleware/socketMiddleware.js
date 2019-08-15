74fd3ff892e4d6d17cdf773427bceac23103de83
shmeegie <ericellb@gmail.com>
2019-08-14 23:51:46 -0400

    Socket listener of a per server basis.

--- src/middleware/socketMiddleware.js
+++ src/middleware/socketMiddleware.js
@@ -1,27 +1,40 @@
 import io from 'socket.io-client';
 
-import { SEND_MESSAGE, UPDATE_CHAT } from '../actions/types';
+import { SEND_MESSAGE, UPDATE_CHAT, GET_INITIAL_DATA } from '../actions/types';
 
 export const socketMiddleware = (baseUrl) => {
   return storeAPI => {
     let socket = io(baseUrl);
-
-    // Dispatch chat to reducers when we receive data
-    socket.on("simple-chat", (message) => {
-      storeAPI.dispatch({
-        type: UPDATE_CHAT,
-        payload: message
-      });
-    });
+    let servers = [];
 
     // Emit when SEND_MESSAGE action
     return next => action => {
       if (action.type === SEND_MESSAGE) {
-        socket.emit('simple-chat', action.payload);
+        let server = action.payload.server.split('-')[0];
+        socket.emit(server, action.payload);
         return;
+      }
+      else if (action.type === GET_INITIAL_DATA) {
+        let servers = (Object.keys(action.payload));
+        setSocketListener(socket, storeAPI, servers);
       }
 
       return next(action);
     }
   }
+}
+
+function setSocketListener(socket, storeAPI, servers) {
+  // Create listener for each server
+  servers.forEach((server) => {
+    // Dispatch chat to reducers when we receive data
+    server = server.split('-')[0];
+    console.log(server);
+    socket.on(server, (message) => {
+      storeAPI.dispatch({
+        type: UPDATE_CHAT,
+        payload: message
+      });
+    });
+  })
 }
