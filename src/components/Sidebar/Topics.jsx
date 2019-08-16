54dc6cb72dfe2b9959933152523f4c6826995e9e
shmeegie <ericellb@gmail.com>
2019-08-16 06:25:00 -0400

    Show Server Settings if admin

--- src/components/Sidebar/Topics.jsx
+++ src/components/Sidebar/Topics.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Person, MoreVert } from '@material-ui/icons';
 import { List, ListItem, ListItemAvatar, Avatar, Tooltip, IconButton, Typography, ListItemText, Menu, MenuItem, Slide } from '@material-ui/core';
 import { changeTopic } from '../../actions';
 
 import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+
+const baseUrl = (process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://simple-chat-apix.herokuapp.com');
 
 export default function Topics(props) {
+
 
   // Get State from Redux Store
   const chatStore = useSelector(state => state.chat);
@@ -20,6 +24,19 @@ export default function Topics(props) {
 
   // Local state
   const [anchorEl, setAnchorEl] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  // Check if admin of active server
+  // Will render admin options for server
+  useEffect(() => {
+    async function getAdmin() {
+      let serverId = activeServer.split('-')[1];
+      const response = await axios.get(`${baseUrl}/server/admin?serverId=${serverId}&userId=${user.userId}`);
+      setIsAdmin(response.data);
+    }
+    getAdmin();
+  }, [activeServer])
+
 
   // Handle topic change, and closes drawer if on mobile view
   const handleTopicChange = (topic) => {
@@ -50,23 +67,25 @@ export default function Topics(props) {
       <List className="topic-list">
         <ListItem className="title-container">
           {activeServer.split('-')[0]}
-          {user.isSignedIn ?
-            <Tooltip title="Server Settings" key="server-settings" placement="right" className="tooltip">
-              <IconButton onClick={(e) => handleSettingsClick(e)}> <MoreVert /> </IconButton>
-            </Tooltip>
-            : null
-          }
-          <Menu
-            id="settings-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClick={handleClose}
-            onClose={handleClose}
-          >
-            <MenuItem> Server Id - {activeServer.split('-')[1]} </MenuItem>
-            <MenuItem onClick={() => handleModalShow('server-rename')}> Change Server Name </MenuItem>
-            <MenuItem onClick={() => handleModalShow('channel-create')}> Add Channel </MenuItem>
-          </Menu>
+          {isAdmin ?
+            <React.Fragment>
+              <Tooltip title="Server Settings" key="server-settings" placement="right" className="tooltip">
+                <IconButton onClick={(e) => handleSettingsClick(e)}> <MoreVert /> </IconButton>
+              </Tooltip>
+
+              <Menu
+                id="settings-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClick={handleClose}
+                onClose={handleClose}
+              >
+                <MenuItem> Server Id - {activeServer.split('-')[1]} </MenuItem>
+                <MenuItem onClick={() => handleModalShow('server-rename')}> Change Server Name </MenuItem>
+                <MenuItem onClick={() => handleModalShow('channel-create')}> Add Channel </MenuItem>
+              </Menu>
+            </React.Fragment>
+            : null}
         </ListItem>
         {topics.map((topic, i) => (
           <Slide direction="right" in={true} timeout={200 * (i + 1)} key={topic + activeServer}>
