043c2d9cfaac33d370e8156ef40bca70bd8cd18e
shmeegie <ericellb@gmail.com>
2019-08-17 00:06:07 -0400

    Updates

---
+++ precache-manifest.133df001a02a6fc93522885685f84055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3723ceec3a82fa40682edd7e86094ce",
+    "url": "/Simple-Chat/index.html"
+  },
+  {
+    "revision": "64cb28ab01d0f0ad8a2b",
+    "url": "/Simple-Chat/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "7901233496267d8d1395",
+    "url": "/Simple-Chat/static/css/main.2fc55b86.chunk.css"
+  },
+  {
+    "revision": "64cb28ab01d0f0ad8a2b",
+    "url": "/Simple-Chat/static/js/2.c12b70c6.chunk.js"
+  },
+  {
+    "revision": "7901233496267d8d1395",
+    "url": "/Simple-Chat/static/js/main.44d5ae3b.chunk.js"
+  },
+  {
+    "revision": "de1605a253086a736309",
+    "url": "/Simple-Chat/static/js/runtime~main.29395272.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Simple-Chat/precache-manifest.922011fcfea70043dce761c637c39089.js"
+  "/Simple-Chat/precache-manifest.133df001a02a6fc93522885685f84055.js"
 );
 
 self.addEventListener('message', (event) => {
