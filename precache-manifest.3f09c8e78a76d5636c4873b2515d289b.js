6ff84d4bb8e9ca6c3a16b0fbe7e565e7f900a6d1
shmeegie <ericellb@gmail.com>
2019-08-18 18:15:26 -0400

    Updates

--- precache-manifest.3f09c8e78a76d5636c4873b2515d289b.js
+++ precache-manifest.3f09c8e78a76d5636c4873b2515d289b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf8a730043e812012dd0028393b17387",
+    "revision": "bbfa04a934d6137bdb1ea2af6dfca12e",
     "url": "/React-Discord-Clone/index.html"
   },
   {
-    "revision": "f6da4fbbc4a7a8856eb3",
+    "revision": "c59dc590e489033b0f09",
     "url": "/React-Discord-Clone/static/css/2.b0f64131.chunk.css"
   },
   {
-    "revision": "fea3e18d43fef37befed",
+    "revision": "fc504d62462a0f31570d",
     "url": "/React-Discord-Clone/static/css/main.df587add.chunk.css"
   },
   {
-    "revision": "f6da4fbbc4a7a8856eb3",
-    "url": "/React-Discord-Clone/static/js/2.1eeaec0c.chunk.js"
+    "revision": "c59dc590e489033b0f09",
+    "url": "/React-Discord-Clone/static/js/2.a0d8a090.chunk.js"
   },
   {
-    "revision": "fea3e18d43fef37befed",
-    "url": "/React-Discord-Clone/static/js/main.c94dfc44.chunk.js"
+    "revision": "fc504d62462a0f31570d",
+    "url": "/React-Discord-Clone/static/js/main.4c137d0b.chunk.js"
   },
   {
     "revision": "3caa7863fd9455193c9a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Discord-Clone/precache-manifest.206ead4b36290e032bb9ad2aac03de5c.js"
+  "/React-Discord-Clone/precache-manifest.3f09c8e78a76d5636c4873b2515d289b.js"
 );
 
 self.addEventListener('message', (event) => {
