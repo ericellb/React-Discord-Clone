0a79505b46a54153ecabf807bd06fe0c55ceb4ff
shmeegie <ericellb@gmail.com>
2019-09-01 09:52:09 -0400

    Auth component to Typescript

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,6 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, KeyboardEvent } from 'react';
 import { useDispatch } from 'react-redux';
-import { Paper, Button, Card, CardContent, Typography, CardActionArea, CardMedia, Slide, TextField, Grid, IconButton, Checkbox } from '@material-ui/core';
+import {
+  Paper,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  CardMedia,
+  Slide,
+  TextField,
+  Grid,
+  IconButton,
+  Checkbox
+} from '@material-ui/core';
 import { GroupAdd, Person, ArrowBack } from '@material-ui/icons';
 import axios from '../Api/api';
 
@@ -8,23 +21,22 @@ import createHashHistory from '../../history';
 import { signIn } from '../../actions';
 
 export default function Auth() {
-
-  // Dispatch 
+  // Dispatch
   const dispatch = useDispatch();
 
   // Local state to control Modal Windows + Data fields
   const [mainVisible, setMainVisible] = useState(true);
-  const [mainDirection, setMainDirection] = useState('left');
+  const [mainDirection, setMainDirection]: any = useState('left');
   const [createVisible, setCreateVisible] = useState(false);
-  const [createDirection, setCreateDirection] = useState('left')
+  const [createDirection, setCreateDirection]: any = useState('left');
   const [loginVisible, setLoginVisible] = useState(false);
-  const [loginDirection, setLoginDirection] = useState('left')
+  const [loginDirection, setLoginDirection]: any = useState('left');
   const [userName, setUserName] = useState('');
   const [userNameError, setUserNameError] = useState(false);
-  const [userNameErrorMsg, setUserNameErrorMsg] = useState(false);
+  const [userNameErrorMsg, setUserNameErrorMsg] = useState('');
   const [userPass, setUserPass] = useState('');
   const [userPassError, setUserPassError] = useState(false);
-  const [userPassErrorMsg, setUserPassErrorMsg] = useState(false)
+  const [userPassErrorMsg, setUserPassErrorMsg] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
 
   // Shows the main modal (sets transition directions and views to visible / non visible)
@@ -35,7 +47,7 @@ export default function Auth() {
     setCreateDirection('right');
     setLoginVisible(false);
     setLoginDirection('right');
-  }
+  };
 
   // Handles showing the Join Server window
   const showCreateAccount = () => {
@@ -43,7 +55,7 @@ export default function Auth() {
     setMainDirection('right');
     setCreateVisible(true);
     setMainVisible(false);
-  }
+  };
 
   // Handles showing the Create Server window
   const showLoginAccount = () => {
@@ -51,38 +63,36 @@ export default function Auth() {
     setMainDirection('right');
     setLoginVisible(true);
     setMainVisible(false);
-  }
+  };
 
   // Handles and checks keypress and calls the callback method
-  const handleKeyPress = (e, callBack) => {
-    if (e.key === "Enter") {
+  const handleKeyPress = (e: KeyboardEvent, callBack: Function) => {
+    if (e.key === 'Enter') {
       callBack();
     }
-  }
+  };
 
   // Validates input and calls callback function
-  const handleOnSubmit = (userName, userPass, callBack) => {
+  const handleOnSubmit = (userName: string, userPass: string, callBack: Function) => {
     let error = false;
     if (userName === '') {
       setUserNameError(true);
       setUserNameErrorMsg('Name cannot be empty');
       error = true;
-    }
-    else setUserNameError(false);
+    } else setUserNameError(false);
     if (userPass.length < 6) {
       setUserPassError(true);
       setUserPassErrorMsg('Passwords must be 6 characters');
       error = true;
-    }
-    else setUserPassError(false);
+    } else setUserPassError(false);
 
     if (!error) {
       callBack();
     }
-  }
+  };
 
   // Handles creation of account and calls sign in action
-  const handleCreateAccount = async (userName, userPass) => {
+  const handleCreateAccount = async (userName: string, userPass: string) => {
     try {
       // encode username and userpass - it may have # $ & + ,  / : ; = ? @ [ ]
       userName = encodeURIComponent(userName);
@@ -94,18 +104,17 @@ export default function Auth() {
       }
       dispatch(signIn(response.data));
       createHashHistory.push('/dashboard');
-    }
-    catch (err) {
+    } catch (err) {
       const errorData = err.response.data;
       if (errorData) {
         setUserNameError(true);
         setUserNameErrorMsg(errorData);
       }
     }
-  }
+  };
 
   // Handles login of account and calls sign in action
-  const handleLoginAccount = async (userName, userPass) => {
+  const handleLoginAccount = async (userName: string, userPass: string) => {
     // encode username and userpass - it may have # $ & + ,  / : ; = ? @ [ ]
     userName = encodeURIComponent(userName);
     userPass = encodeURIComponent(userPass);
@@ -117,17 +126,16 @@ export default function Auth() {
       }
       dispatch(signIn(response.data));
       createHashHistory.push('/dashboard');
-    }
-    catch (err) {
+    } catch (err) {
       const errorData = err.response.data;
       if (errorData) {
         setUserNameError(true);
         setUserNameErrorMsg(errorData);
         setUserPassError(true);
-        setUserPassErrorMsg(errorData)
+        setUserPassErrorMsg(errorData);
       }
     }
-  }
+  };
 
   // Renders options to Create or Login to account
   const renderMain = () => {
@@ -135,18 +143,26 @@ export default function Auth() {
       <Slide direction={mainDirection} in={mainVisible} timeout={350} mountOnEnter unmountOnExit>
         <Grid container spacing={3} justify="center" alignItems="center">
           <Grid item sm={12} xs={12}>
-            <Typography variant="h5" color="primary" align="center">Create an account, or sign in!</Typography>
+            <Typography variant="h5" color="primary" align="center">
+              Create an account, or sign in!
+            </Typography>
           </Grid>
           <Grid item sm={6} xs={12}>
             <Card className="grid-card">
               <CardActionArea onClick={() => showCreateAccount()}>
                 <CardContent>
-                  <Typography variant="h5" color="primary" gutterBottom>Create</Typography>
-                  <Typography variant="body1" paragraph>Create a new account.</Typography>
+                  <Typography variant="h5" color="primary" gutterBottom>
+                    Create
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    Create a new account.
+                  </Typography>
                   <CardMedia>
                     <GroupAdd className="modal-card-icon" />
                   </CardMedia>
-                  <Button variant="contained" color="primary">Create</Button>
+                  <Button variant="contained" color="primary">
+                    Create
+                  </Button>
                 </CardContent>
               </CardActionArea>
             </Card>
@@ -155,42 +171,56 @@ export default function Auth() {
             <Card className="grid-card">
               <CardActionArea onClick={() => showLoginAccount()}>
                 <CardContent>
-                  <Typography variant="h5" color="secondary" gutterBottom>Login</Typography>
-                  <Typography variant="body1" paragraph>Sign in to an existing account.</Typography>
+                  <Typography variant="h5" color="secondary" gutterBottom>
+                    Login
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    Sign in to an existing account.
+                  </Typography>
                   <CardMedia>
                     <Person className="modal-card-icon" />
                   </CardMedia>
-                  <Button variant="contained" color="secondary">Login </Button>
+                  <Button variant="contained" color="secondary">
+                    Login{' '}
+                  </Button>
                 </CardContent>
               </CardActionArea>
             </Card>
           </Grid>
         </Grid>
       </Slide>
-    )
-  }
+    );
+  };
 
   // Renders the form to create an account
   const renderCreateAccount = () => {
     return (
-      <Slide direction={createDirection} in={createVisible} timeout={350} mountOnEnter unmountOnExit >
+      <Slide direction={createDirection} in={createVisible} timeout={350} mountOnEnter unmountOnExit>
         <Grid container spacing={1} justify="center" alignItems="center">
           <Grid item xs={12}>
-            <IconButton onClick={showMain}><ArrowBack /></IconButton>
-            <Typography variant="h5" color="primary" align="center">Create Account</Typography>
+            <IconButton onClick={showMain}>
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h5" color="primary" align="center">
+              Create Account
+            </Typography>
           </Grid>
           <Grid item xs={12} className="grid-textfield">
             <TextField
               id="username"
               label="Username"
-              values={userName}
+              value={userName}
               error={userNameError}
               helperText={userNameErrorMsg}
-              onChange={(e) => setUserName(e.target.value)}
+              onChange={e => setUserName(e.target.value)}
               margin="dense"
               autoComplete="off"
               variant="outlined"
-              onKeyPress={(e) => handleKeyPress(e, () => handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass)))}
+              onKeyPress={e =>
+                handleKeyPress(e, () =>
+                  handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass))
+                )
+              }
             />
           </Grid>
           <Grid item xs={12} className="grid-textfield">
@@ -198,26 +228,36 @@ export default function Auth() {
               id="password"
               label="Password"
               type="password"
-              values={userPass}
+              value={userPass}
               error={userPassError}
               helperText={userPassErrorMsg}
-              onChange={(e) => setUserPass(e.target.value)}
+              onChange={e => setUserPass(e.target.value)}
               margin="dense"
               autoComplete="off"
               variant="outlined"
-              onKeyPress={(e) => handleKeyPress(e, () => handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass)))}
+              onKeyPress={e =>
+                handleKeyPress(e, () =>
+                  handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass))
+                )
+              }
             />
           </Grid>
           <Grid item xs={12} className="grid-button">
             <div>
-              Remember Me <Checkbox value={rememberMe} onChange={((e) => setRememberMe(e.target.checked))} />
+              Remember Me <Checkbox value={rememberMe} onChange={e => setRememberMe(e.target.checked)} />
             </div>
-            <Button variant="contained" color="primary" onClick={() => handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass))}>Create</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleOnSubmit(userName, userPass, () => handleCreateAccount(userName, userPass))}
+            >
+              Create
+            </Button>
           </Grid>
         </Grid>
-      </Slide >
-    )
-  }
+      </Slide>
+    );
+  };
 
   // Renders the form to login to account
   const renderLoginAccount = () => {
@@ -225,21 +265,29 @@ export default function Auth() {
       <Slide direction={loginDirection} in={loginVisible} timeout={350} mountOnEnter unmountOnExit>
         <Grid container spacing={2} justify="center" alignItems="center">
           <Grid item xs={12}>
-            <IconButton onClick={showMain}><ArrowBack /></IconButton>
-            <Typography variant="h5" color="primary" align="center">Login Account</Typography>
+            <IconButton onClick={showMain}>
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h5" color="primary" align="center">
+              Login Account
+            </Typography>
           </Grid>
           <Grid item xs={12} className="grid-textfield">
             <TextField
               id="username"
               label="Username"
-              values={userName}
+              value={userName}
               error={userNameError}
               helperText={userNameErrorMsg}
-              onChange={(e) => setUserName(e.target.value)}
+              onChange={e => setUserName(e.target.value)}
               margin="dense"
               autoComplete="off"
               variant="outlined"
-              onKeyPress={(e) => handleKeyPress(e, () => handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass)))}
+              onKeyPress={e =>
+                handleKeyPress(e, () =>
+                  handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass))
+                )
+              }
             />
           </Grid>
           <Grid item xs={12} className="grid-textfield">
@@ -247,26 +295,37 @@ export default function Auth() {
               id="password"
               label="Password"
               type="password"
-              values={userPass}
+              value={userPass}
               error={userPassError}
               helperText={userPassErrorMsg}
-              onChange={(e) => setUserPass(e.target.value)}
+              onChange={e => setUserPass(e.target.value)}
               margin="dense"
               autoComplete="off"
               variant="outlined"
-              onKeyPress={(e) => handleKeyPress(e, () => handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass)))}
+              onKeyPress={e =>
+                handleKeyPress(e, () =>
+                  handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass))
+                )
+              }
             />
           </Grid>
           <Grid item xs={12} className="grid-button">
             <div>
-              Remember Me <Checkbox vale={rememberMe} onChange={((e) => setRememberMe(e.target.checked))} />
+              Remember Me <Checkbox value={rememberMe} onChange={e => setRememberMe(e.target.checked)} />
             </div>
-            <Button className="modal-login-button" variant="contained" color="primary" onClick={() => handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass))}>Login</Button>
+            <Button
+              className="modal-login-button"
+              variant="contained"
+              color="primary"
+              onClick={() => handleOnSubmit(userName, userPass, () => handleLoginAccount(userName, userPass))}
+            >
+              Login
+            </Button>
           </Grid>
         </Grid>
-      </Slide >
-    )
-  }
+      </Slide>
+    );
+  };
 
   return (
     <div className="auth-wrapper">
@@ -274,7 +333,7 @@ export default function Auth() {
         {renderMain()}
         {renderCreateAccount()}
         {renderLoginAccount()}
-      </Paper >
-    </div >
-  )
+      </Paper>
+    </div>
+  );
 }
